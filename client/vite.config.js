2f1cd99d0ef462496868a6d9d13279a7db128366
Sarthak Ameriya <sarthakameriya@gmail.com>
2026-04-29 15:15:12 +0530

    Switch to vite-plugin-monaco-editor-esm to fix build error

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { createRequire } from 'module'
-
-const require = createRequire(import.meta.url)
-const monacoEditorPlugin = require('vite-plugin-monaco-editor')
+import monacoEditorPlugin from 'vite-plugin-monaco-editor-esm'
 
 export default defineConfig({
   plugins: [
