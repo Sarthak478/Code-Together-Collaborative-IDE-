3ec50e7542bc662c99a25b478faa2a6b78d10075
Sarthak Ameriya <sarthakameriya@gmail.com>
2026-04-29 15:02:02 +0530

    frontend deploying1

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,14 +3,12 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import monacoEditorPlugin from 'vite-plugin-monaco-editor'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    (monacoEditorPlugin.default || monacoEditorPlugin)({
+    monacoEditorPlugin({
       languageWorkers: ['editorWorkerService', 'typescript', 'json', 'css', 'html']
     })
   ]
 })
-
