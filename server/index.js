667d443c3508115f8dda383419e856f013dcdd59
Sarthak Ameriya <sarthakameriya@gmail.com>
2026-06-24 15:46:54 +0530

    again a commit

--- server/index.js
+++ server/index.js
@@ -89,7 +89,7 @@ app.use(helmet({
     contentSecurityPolicy: {
         directives: {
             "default-src": ["'self'"],
-            "script-src": ["'self'", "'unsafe-inline'", "'unsafe-eval'", "blob:", "https://cdn.jsdelivr.net"],
+            "script-src": ["'self'", "'unsafe-inline'", "'unsafe-eval'", "blob:", "https://cdn.jsdelivr.net", "data:"],
             "style-src": ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
             "font-src": ["'self'", "https://fonts.gstatic.com"],
             "connect-src": [
